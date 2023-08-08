5a9117cda2fdd624e60793b2ba3a1d8f494f1703
Sulaimon Azeez Ayodeji <sulaimonaa@gmail.com>
2023-08-08 08:40:25 +0100

    add empty navigation

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,14 @@
-const Header = () => <h1>Header component</h1>;
+import styles from '../styles.module.css';
+import Logo from '../assets/planet.png';
+
+const Header = () => (
+  <div className={`${styles.container_fluid} ${styles.header}`}>
+    <nav className={`${styles.container} ${styles.navbar}`}>
+      <div className={styles.brand}>
+        <img src={Logo} alt="logo" />
+      </div>
+    </nav>
+  </div>
+);
 
 export default Header;
